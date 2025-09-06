c46dc346ab1b9a8a1a04ffa6750bf8b254314c33
Harry Jupiter <72943529+harryjupiter148@users.noreply.github.com>
2025-09-06 04:50:16 +0100

    Update login-form.js

--- login-form.js
+++ login-form.js
@@ -1,29 +1,7 @@
-// const form = document.getElementById("loginForm");
-// const btnH = document.getElementById("btn");
-// const loginBox = document.getElementById("login-box");
-// const message = document.getElementById("message");
-// let pStyle = document.getElementById("message");
-// let body = document.getElementById("body");
-// let h2 = document.getElementById("h2");
 let btn = document.getElementById("btn");
 let input1 = document.getElementById("email");
 let input2 = document.getElementById("username");
 let input3 = document.getElementById("password");
-
-// form.addEventListener("submit", function (event) {
-//   event.preventDefault();
-
-//   const username = document.getElementById("username").value.trim();
-//   const password = document.getElementById("password").value.trim();
-
-//   if (username === "admin" && password === "1234") {
-//     message.style.color = "green";
-//     message.textContent = "Login successful!";
-//   } else {
-//     message.style.color = "red";
-//     message.textContent = "Invalid username or password";
-//   }
-// });
 
 function press() {
   input1.style.border = "2px solid blue";
@@ -34,58 +12,7 @@ function press() {
   input3.style.transition = "border 0.5s ease";
   btn.style.backgroundColor = "green";
   btn.style.transition = "background-color 0.5s ease";
+  console.log(input1.value);
+  console.log(input2.value);
+  console.log(input3.value);
 }
-
-// pStyle.style.textAlign = "center";
-// pStyle.style.marginTop = "10px";
-
-// body.style.fontFamily = "Arial", "san-serif";
-// body.style.display = "flex";
-// body.style.height = "100vh";
-// body.style.justifyContent = "center";
-// body.style.alignItems = "center";
-// body.style.background = "#f4f4f4";
-
-// loginBox.style.background = "#fff";
-// loginBox.style.padding = "20px";
-// loginBox.style.borderRadius = "10px";
-// loginBox.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
-// loginBox.style.width = "300px";
-
-// h2.style.textAlign = "center";
-// h2.style.marginBottom = "20px";
-
-// input1.style.width = "80%";
-// input1.style.padding = "10px 0px 10px 10px";
-// input1.style.margin = "2px 0px 8px 0px";
-// input1.style.border = "1px solid #ccc";
-// input1.style.borderRadius = "5px";
-
-// input2.style.width = "80%";
-// input2.style.padding = "10px 0px 10px 10px";
-// input2.style.margin = "2px 0px 8px 0px";
-// input2.style.border = "1px solid #ccc";
-// input2.style.borderRadius = "5px";
-
-// input3.style.width = "80%";
-// input3.style.padding = "10px 0px 10px 10px";
-// input3.style.margin = "2px 0px 8px 0px";
-// input3.style.border = "1px solid #ccc";
-// input3.style.borderRadius = "5px";
-
-// btn.style.width = "100px";
-// btn.style.padding = "10px";
-// btn.style.marginTop ="8px";
-// btn.style.background = "#4CAF50";
-// btn.style.border = "none";
-// btn.style.borderRadius = "5px";
-// btn.style.cursor = "pointer";
-// btn.style.transition = "background 0.2s ease";
-
-// btnH.addEventListener('mouseenter', () => {
-//    btnH.style.background = "#45a049";
-// });
-// btnH.addEventListener("mouseleave", () => {
-//    btnH.style.background = "#4CAF50";
-// });
-
